fa97ec501f72a55b0c156c3d5edf272b034762cf
giusepeople <giuseppe.cozza@peopledesign.it>
2026-07-07 16:00:38 +0200

    added backend

---
+++ Sito/backend/src/test/java/com/pdfactory/pdf_spring/PdfSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdfactory.pdf_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
